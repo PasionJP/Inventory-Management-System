e3b5b3b4b86b1b323e23acb0dda2a0a5d7d76a1b
John Paul Pasion <johnpaulpasionbricks01@gmail.com>
2021-01-31 19:48:33 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Login_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bgPicture = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernamelabel = new System.Windows.Forms.Label();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.passwordlabel = new System.Windows.Forms.Label();
+            this.signin = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bgPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bgPicture
+            // 
+            this.bgPicture.Image = ((System.Drawing.Image)(resources.GetObject("bgPicture.Image")));
+            this.bgPicture.Location = new System.Drawing.Point(-7, -5);
+            this.bgPicture.Name = "bgPicture";
+            this.bgPicture.Size = new System.Drawing.Size(415, 382);
+            this.bgPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bgPicture.TabIndex = 0;
+            this.bgPicture.TabStop = false;
+            this.bgPicture.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Russo One", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(133)))), ((int)(((byte)(254)))));
+            this.label1.Location = new System.Drawing.Point(434, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LOGIN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // usernamelabel
+            // 
+            this.usernamelabel.AutoSize = true;
+            this.usernamelabel.BackColor = System.Drawing.Color.White;
+            this.usernamelabel.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernamelabel.Location = new System.Drawing.Point(388, 126);
+            this.usernamelabel.Name = "usernamelabel";
+            this.usernamelabel.Size = new System.Drawing.Size(108, 23);
+            this.usernamelabel.TabIndex = 2;
+            this.usernamelabel.Text = "Username";
+            this.usernamelabel.Click += new System.EventHandler(this.usernamelabel_Click);
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(392, 161);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(217, 26);
+            this.usernameBox.TabIndex = 3;
+            this.usernameBox.TextChanged += new System.EventHandler(this.usernameBox_TextChanged);
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(392, 239);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(217, 26);
+            this.passwordBox.TabIndex = 5;
+            this.passwordBox.UseSystemPasswordChar = true;
+            // 
+            // passwordlabel
+            // 
+            this.passwordlabel.AutoSize = true;
+            this.passwordlabel.BackColor = System.Drawing.Color.White;
+            this.passwordlabel.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordlabel.Location = new System.Drawing.Point(388, 204);
+            this.passwordlabel.Name = "passwordlabel";
+            this.passwordlabel.Size = new System.Drawing.Size(103, 23);
+            this.passwordlabel.TabIndex = 4;
+            this.passwordlabel.Text = "Password";
+            // 
+            // signin
+            // 
+            this.signin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(133)))), ((int)(((byte)(254)))));
+            this.signin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.signin.Location = new System.Drawing.Point(442, 301);
+            this.signin.Name = "signin";
+            this.signin.Size = new System.Drawing.Size(130, 46);
+            this.signin.TabIndex = 6;
+            this.signin.Text = "SIGN IN";
+            this.signin.UseVisualStyleBackColor = false;
+            this.signin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Russo One", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.title.Location = new System.Drawing.Point(12, 63);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(340, 58);
+            this.title.TabIndex = 7;
+            this.title.Text = "INVENTORY MANAGEMENT\r\nSYSTEM";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.title.Click += new System.EventHandler(this.title_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(652, 377);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.signin);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.passwordlabel);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.usernamelabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bgPicture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bgPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bgPicture;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label usernamelabel;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label passwordlabel;
+        private System.Windows.Forms.Button signin;
+        private System.Windows.Forms.Label title;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            title.Parent = bgPicture;
+            title.BackColor = Color.Transparent;
+            this.AcceptButton = signin;
+        }
+
+        private void usernamelabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void usernameBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void title_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (usernameBox.Text == "" || passwordBox.Text == "")
+            {
+                MessageBox.Show("Please enter both username and password");
+            }
+            else
+            {
+                SqlConnection sc = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\johnp\Documents\Database.mdf;Integrated Security=True;Connect Timeout=30");
+                string query = "Select * From [Table] where username = '" + usernameBox.Text.Trim() + "' and password = '" + passwordBox.Text.Trim() + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(query, sc);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows.Count == 1)
+                {
+                    this.Hide();
+                    IMS ss = new IMS();
+                    ss.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username and password! Please try again.");
+                }
+            }
+        }
+    }
+}
